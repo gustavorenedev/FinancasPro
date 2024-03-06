26405005d1435e4f313a09a45fb1343240b9f8e5
Gustavo René <renegustavo42@gmail.com>
2024-03-05 22:46:10 -0300

    Finish endpoints category

---
+++ src/main/java/br/com/fiap/financaspro/model/Categoria.java
@@ -0,0 +1,15 @@
+package br.com.fiap.financaspro.model;
+
+import java.util.Random;
+
+// imutável
+public record Categoria (Long id, String nome, String icone){
+    
+    public Categoria(Long id, String nome, String icone){
+        var key = (id == null) ? Math.abs( new Random().nextLong() ) : id;
+        this.id = key;
+        this.nome = nome;
+        this.icone = icone;
+    }
+    
+}
